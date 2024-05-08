71fa16e9fcc1bc86ea72567f07b4530b5770f086
Harshit Bole <harshitbole@Harshits-MacBook-Air.local>
2024-05-08 20:27:19 +0530

    Initial spring boot setup

--- SpringBoot/src/main/java/com/example/springboot/Application.java
+++ SpringBoot/src/main/java/com/example/springboot/Application.java
@@ -2,12 +2,16 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        Alien alien = context.getBean(Alien.class);
+        alien.code();
     }
 
 }
